3d1587a576b1a82b74d01addee97d1154a262d28
Robert Meredith <64670585+robertmeredith@users.noreply.github.com>
2024-06-20 16:41:25 +1000

    started work on creating contact modal

---
+++ features/common/contact-modal/contact-modal.tsx
@@ -0,0 +1,9 @@
+import styles from "./contact-modal.module.scss";
+
+export const ContactModal = () => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}></div>
+    </div>
+  );
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,25 @@
 import styles from "./index.module.scss";
 import { LandingNavigation } from "@features/layout";
+import { useState } from "react";
+import { ContactModal } from "@features/common";
 
 const LandingPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  console.log("showModal", showModal);
+
   return (
     <div>
       <LandingNavigation />
+      <ContactModal />
       <button
         className={styles.contactButton}
-        onClick={() =>
-          alert(
-            "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
-          )
-        }
+        onClick={() => setShowModal(!showModal)}
+        // onClick={() =>
+        //   alert(
+        //     "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
+        //   )
+        // }
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/message.svg" alt="Contact" />
