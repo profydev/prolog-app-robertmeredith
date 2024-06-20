8fdec6b0a3cfe51849046951943b0a3b884f52f9
Robert Meredith <64670585+robertmeredith@users.noreply.github.com>
2024-06-20 17:37:30 +1000

    implemented landing page contact modal design

--- features/common/contact-modal/contact-modal.tsx
+++ features/common/contact-modal/contact-modal.tsx
@@ -1,9 +1,47 @@
 import styles from "./contact-modal.module.scss";
+import { Button, ButtonColor, ButtonSize } from "@features/ui";
 
-export const ContactModal = () => {
+type ContactModalProps = {
+  closeModal: () => void;
+  openEmail: () => void;
+};
+
+export const ContactModal = ({ closeModal, openEmail }: ContactModalProps) => {
   return (
-    <div className={styles.modal}>
-      <div className={styles.content}></div>
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <div className={styles.content}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/icons/mail.svg" alt="email icon" className={styles.icon} />
+          <div className={styles.textContainer}>
+            <h2>Contact Us Via Email</h2>
+            <div className={styles.supporting}>
+              Any questions? Send us an email at prolog@profy.dev. We usually
+              answer within 24 hours.
+            </div>
+          </div>
+        </div>
+        {/* ACTIONS */}
+        <div className={styles.actions}>
+          <Button
+            size={ButtonSize.lg}
+            color={ButtonColor.gray}
+            className={styles.button}
+            onClick={closeModal}
+          >
+            Cancel
+          </Button>
+
+          <Button
+            size={ButtonSize.lg}
+            color={ButtonColor.primary}
+            className={styles.button}
+            onClick={openEmail}
+          >
+            Open Email App
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const LandingPage = () => {
   return (
     <div>
       <LandingNavigation />
-      <ContactModal />
+      {showModal && <ContactModal closeModal={() => setShowModal(false)} />}
       <button
         className={styles.contactButton}
         onClick={() => setShowModal(!showModal)}
