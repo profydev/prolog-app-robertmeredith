0d09989afc438577f58e639c9939f67f9cc2b10c
Robert Meredith <64670585+robertmeredith@users.noreply.github.com>
2024-06-21 10:42:35 +1000

    fixed prettier issue in /common/index.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,11 +20,6 @@ const LandingPage = () => {
       <button
         className={styles.contactButton}
         onClick={() => setShowModal(!showModal)}
-        // onClick={() =>
-        //   alert(
-        //     "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
-        //   )
-        // }
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/message.svg" alt="Contact" />
